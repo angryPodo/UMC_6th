3f12473aecf4f272305da013a5559f7f5064d2ad
Angrypodo <hanminjae131@gmail.com>
2024-06-02 19:17:57 +0900

    add 404 status

--- config/responseStatus.js
+++ config/responseStatus.js
@@ -3,6 +3,9 @@ import { StatusCodes } from "http-status-codes";
 export const status = {
   SUCCESS: { status: StatusCodes.OK, "isSuccecc": true, "code": 2000, "message": "Success congraturation!!" },
 
+  //404 error
+  NOT_FOUND: { status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "COMMON005", "message": "요청한 페이지를 찾을 수 없습니다. 관리자에게 문의 바랍니다." },
+
   INTERNAL_SERVER_ERROR: { status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "COMMON000", "message": "서버 에러, 관리자에게 문의 바랍니다." },
   BAD_REQUEST: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "COMMON001", "message": "잘못된 요청입니다." },
   UNAUTHORIZED: { status: StatusCodes.UNAUTHORIZED, "isSuccess": false, "code": "COMMON002", "message": "권한이 잘못되었습니다." },
