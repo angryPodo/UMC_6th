8f239f5309479e882932fed105ecf64490e8bb2a
Angrypodo <hanminjae131@gmail.com>
2024-06-02 14:09:50 +0900

    edit BaseError

--- services/tempService.js
+++ services/tempService.js
@@ -1,5 +1,7 @@
 import { tempResponseDTO } from "../dtos/tempResponseDTO"
 import { flagResponseDTO } from "../dtos/tempResponseDTO";
+import { BaseError } from "../config/error";
+import { status } from "../config/responseStatus";
 
 export const getTempData = () => {
   return tempResponseDTO("This is TEST! >.0")
@@ -7,7 +9,7 @@ export const getTempData = () => {
 
 export function CheckFlag(flag) {
   if (flag == 1)
-    throw new Error("Flag is 1!!");
+    throw new BaseError(status.BAD_REQUEST);
   else {
     return flagResponseDTO(flag);
   }
